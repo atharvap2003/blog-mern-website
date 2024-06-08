64193e846bca64c3345caec251fb0a3333ba14a6
Atharva Pandharikar <atharvapandharikar5@gmail.com>
2024-06-08 11:23:21 +0530

    Inital Setup:: developed home, login, register :/ backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Post from "./Post";
+import Header from "./Header";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <>
+      <form action="" className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </>
+  );
+}
